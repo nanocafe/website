76284912779880a8e526c94576af6796d8c7849d
Saizo <85983166+saizo-nanocafe@users.noreply.github.com>
2021-10-31 18:55:47 -0400

    nanocafe eom announcement fix

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -16,7 +16,8 @@ const Container = styled.main`
 
 export const HomeScreen: React.FC = () => {
   return <Container>
-    <h2>Nanocafe EOM beta release information coming soon!</h2>
+<p style="background-color:#FF0000; color: #FFFFFF
+; text-align:center; font-weight:light;">Nanocafe EOM beta release information coming soon!</p>
     <Visualization/>
     <News/>
   </Container>;

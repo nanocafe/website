02be371c817c9baf7a26e3d4e5d4145c170b255e
Saizo <85983166+saizo-nanocafe@users.noreply.github.com>
2022-01-24 22:18:45 -0500

    ticker notice update

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -13,9 +13,18 @@ const Container = styled.main`
     text-align: center;
   }
 `;
+ const Paragraph = styled.main`
+  color: #000;
+  text-align: center;
+  margin-top: 2rem;
+  font-size: 16px;
+  padding: 5px;
+  background-color: #F3695F;
+`; 
 
 export const HomeScreen: React.FC = () => {
   return <Container>
+    <Paragraph>Ticker data is currently not reflecting xno/usdt & xno/btc market rates due to reliance on the binance api, they will be updated ASAP to reflect market rates once binance completes their ticker change maintenance for nano/xno by 1/28.</Paragraph>
     <Visualization/>
     <News/>
   </Container>;

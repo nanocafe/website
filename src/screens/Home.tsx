fec7b5a2f07eefd6da6f7ed373c9d587046acbd9
Saizo <85983166+saizo-nanocafe@users.noreply.github.com>
2022-01-28 12:42:37 -0500

    notice removal

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -13,18 +13,9 @@ const Container = styled.main`
     text-align: center;
   }
 `;
- const Paragraph = styled.main`
-  color: #000;
-  text-align: center;
-  margin-top: 2rem;
-  font-size: 16px;
-  padding: 5px;
-  background-color: #F3695F;
-`; 
 
 export const HomeScreen: React.FC = () => {
   return <Container>
-    <Paragraph>Ticker data for XNO:USDT & XNO:BTC are currently not working due to reliance on the binance API, tickers will be fixed ASAP once binance finishes their scheduled ticker maintenance for nano/xno by 1/28. </Paragraph>
     <Visualization/>
     <News/>
   </Container>;

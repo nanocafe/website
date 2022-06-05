8ef15c3204168b9ca4d70bdb4a9383d0f952da21
awaisspk <iamawaisiqball@gmail.com>
2022-06-05 22:00:58 +0500

    add fx rates marquee

--- src/screens/Charts.tsx
+++ src/screens/Charts.tsx
@@ -1,22 +1,24 @@
-import {css} from '@emotion/css';
+import { css } from "@emotion/css";
 // import styled from '@emotion/styled';
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useState } from "react";
 // import {BsQuestionDiamond} from 'react-icons/bs';
 // import {IoConstructOutline} from 'react-icons/io5';
-import {AdvancedRealTimeChart} from "react-ts-tradingview-widgets";
-import axios from 'axios';
-import { subDays } from 'date-fns'
+import { AdvancedRealTimeChart } from "react-ts-tradingview-widgets";
+import axios from "axios";
+import { subDays } from "date-fns";
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+// @ts-ignore
+import { Currencies } from "../components/Currencies";
 
 // const Container = styled.main`
 //   flex: 1;
@@ -36,7 +38,6 @@ import { Line } from 'react-chartjs-2';
 // `;
 
 const construction = css`
-
   padding: 3rem;
   //height: 600px;
 
@@ -45,7 +46,7 @@ const construction = css`
   }
 
   @media only screen and (max-width: 500px) {
-    padding: .5rem;
+    padding: 0.5rem;
   }
 
   h2 {
@@ -55,6 +56,12 @@ const construction = css`
 
   p {
     margin-top: 0.5rem;
+  }
+  .currencies-container h1 {
+    margin-top: 1.5rem;
+    @media only screen and (min-width: 600px) {
+      margin-top: 0px;
+    }
   }
 
   .under-construction-container {
@@ -77,133 +84,127 @@ const construction = css`
 
     .left-side {
       min-height: 20rem;
-      //display: flex;
-      //justify-content: stretch;
-      //align-content: stretch;
+      // display: flex;
+      // justify-content: stretch;
+      // align-content: stretch;
     }
 
     .right-side {
       min-height: 30rem;
       text-align: center;
-      //display: flex;
-      //justify-content: stretch;
-      //align-content: center;
+      // display: flex;
+      // justify-content: stretch;
+      // align-content: stretch;
     }
-    
   }
 `;
 
-
-
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
 );
 
 const options = {
-    responsive: true,
-    plugins: {
-        legend: {
-            position: 'top' as const,
-        },
-        title: {
-            display: true,
-            text: 'XNO Market Cap Chart',
-        },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
     },
+    title: {
+      display: true,
+      text: "XNO Market Cap Chart",
+    },
+  },
 };
 
 export const ChartsScreen: React.FC = () => {
+  const [data, setData] = useState<any>([]);
 
-    const [data,setData] = useState<any>([]);
+  let prev30Days: any = Array.from(Array(365).keys()).reverse();
 
-    let prev30Days: any = Array.from(Array(365).keys()).reverse();
+  const d = new Date();
 
-    const d = new Date();
+  prev30Days = prev30Days.map((val: any) => {
+    const date = subDays(d, val);
+    return date.toLocaleDateString();
+  });
 
-    prev30Days = prev30Days.map((val:any) => {
-        const date = subDays(d,val);
-        return date.toLocaleDateString()
-    })
+  console.log(prev30Days, "Prev 30 days");
 
-    console.log(prev30Days,'Prev 30 days')
+  const chartData = {
+    labels: prev30Days,
+    datasets: [
+      {
+        label: "XNO",
+        data: data,
+        // data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
 
-    const chartData = {
-        labels:prev30Days,
-        datasets: [
-            {
-                label: 'XNO',
-                data: data,
-                // data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-                borderColor: 'rgb(255, 99, 132)',
-                backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            }
-        ]
-    }
+  //todo - fetch the data
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/nano/market_chart?vs_currency=usd&days=364&interval=daily",
+        {}
+      )
+      .then((res) => {
+        console.log(res.data.market_caps);
 
-    //todo - fetch the data
-    useEffect(() => {
+        console.log(res, "response data");
 
-        axios.get('https://api.coingecko.com/api/v3/coins/nano/market_chart?vs_currency=usd&days=364&interval=daily',{})
-            .then(res => {
-            console.log(res.data.market_caps);
+        setData(res.data.market_caps);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
 
-            console.log(res,'response data')
-
-            setData(res.data.market_caps)
-
-        }).catch(err => {
-            console.log(err.message);
-        })
-
-    },[])
-
-
-    return (
-        <div className={construction}>
-
-
-            <div className="charts-container">
-
-                {/* Left side*/}
-                <div className={'left-side'}>
-
-                    <AdvancedRealTimeChart
-                        locale={'en'}
-                        timezone={'Etc/UTC'}
-                        interval={'60'}
-                        symbol={'BINANCE:XNOUSDT'}
-                        theme="dark"
-                        autosize
-                        hide_side_toolbar={true}
-                        // toolbar_bg={'#f1f3f6'}
-                        enable_publishing={false}
-                        style={'1'}
-                        // watchlist={["BINANCE:XNOETH",
-                        //     "BINANCE:XNOBTC",
-                        //     "BITHUMB:XNOKRW",
-                        //     "BINANCE:XNOUSDT"
-                        // ]}
-                        // calendar={true}
-                        container_id={'tradingview_1904b'}
-                    />
-
-                </div>
-
-                {/* Right Side*/}
-                <div className={'right-side'}>
-
-                    {/*<div>*/}
-                        <Line height={210} options={options} data={chartData}/>
-                    {/*</div>*/}
-                </div>
-
-            </div>
+  return (
+    <div className={construction}>
+      <div className="currencies-container">
+        <h1 style={{ fontSize: "1.2rem" }}>XNO Fx Rates</h1>
+        <Currencies />
+      </div>
+      <div className="charts-container">
+        {/* Left side*/}
+        <div className={"left-side"}>
+          <AdvancedRealTimeChart
+            locale={"en"}
+            timezone={"Etc/UTC"}
+            interval={"60"}
+            symbol={"BINANCE:XNOUSDT"}
+            theme="dark"
+            autosize
+            hide_side_toolbar={true}
+            // toolbar_bg={'#f1f3f6'}
+            enable_publishing={false}
+            style={"1"}
+            // watchlist={["BINANCE:XNOETH",
+            //     "BINANCE:XNOBTC",
+            //     "BITHUMB:XNOKRW",
+            //     "BINANCE:XNOUSDT"
+            // ]}
+            // calendar={true}
+            container_id={"tradingview_1904b"}
+          />
         </div>
-    )
-}
+
+        {/* Right Side*/}
+        <div className={"right-side"}>
+          {/*<div>*/}
+          <Line height={210} options={options} data={chartData} />
+          {/*</div>*/}
+        </div>
+      </div>
+    </div>
+  );
+};

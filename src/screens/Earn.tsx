901b498a8dfcb5a58c9a5d8fde35ca133f07c5c7
saizo-nanocafe <saizo@nanocafe.cc>
2023-01-26 00:43:30 -0500

    earn update

--- src/screens/Earn.tsx
+++ src/screens/Earn.tsx
@@ -6,44 +6,75 @@ import { IoConstructOutline } from 'react-icons/io5';
 
 
 const Container = styled.main`
-  flex: 1;
-  
-  div {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(2, minmax(250px, auto));
+  grid-row-gap: 1rem;
+  width: 100%;
+  margin: auto;
+  background-color: #3C3939;
+  @media (min-width: 600px) {
+    .Banner {
+      width: 50%;
+    }
+  }
+  `;
+
+const LargeBanner = styled.a`
+  grid-column: 1/4;
+  text-decoration: none;
+  color: inherit;
+  text-align: center;
+  padding: 2rem;
+  margin-top: 1rem;
+  &:hover {
+    /* background-color: #E0E0E0; you can change the color as you like */
+  }
+
+  img {
+    max-width: 100%;
+  }
+
+  h2 {
+    font-size: 2rem;
     margin-top: 1rem;
-    text-align: left;
   }
 `;
 
-const earnDesign = css`
-  text-align: left;
-  padding: 3rem;
-  height: 600px;
-  h2 {
-    margin-top: 1rem;
-    font-size: 2rem;
+const SmallBanner = styled.a`
+  grid-column: 1/4;
+  text-decoration: none;
+  color: inherit;
+  text-align: center;
+  padding: 2rem;
+
+  &:hover {
+    /* background-color: #E0E0E0; you can change the color as you like */
   }
+
   img {
-  max-width: 100%;
+    max-width: 100%;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    margin-top: 1rem;
   }
 `;
 
 export const EarnScreen: React.FC = () => {
-  return <div className={earnDesign}>
-      
+  return (
+    <Container>
+      <LargeBanner class="Banner" href="#" title="This will be available February 1st!">
+        <img src={require('url:../../assets/febreward.png')} />
+      </LargeBanner>
+      <SmallBanner class="Banner">      <a href="https://playnano.online/?ref=nano_3odatubif8zuemhgtmdh465somyy9hmdeab6sa15od79cbdu79zgnndx7ozk" title="Affiliate Link">
+        <img src={require('url:../../assets/newplaynano.png')} />
+      </a></SmallBanner>
 
-{/*     <a href="#" title="This will be available February 1st!">
-     <img src={require('url:../../assets/febreward.png')} />
-   </a> */}
-  
-    <br></br>
-   <a href="https://playnano.online/?ref=nano_3odatubif8zuemhgtmdh465somyy9hmdeab6sa15od79cbdu79zgnndx7ozk" title="Affiliate Link">
-     <img src={require('url:../../assets/playnano.png')} />
-   </a>
-   <br></br>
-    <br></br>
-   <a href="https://cryptovision.live" title="Cryptovision.live">
-     <img src={require('url:../../assets/cvn.png')} />
-   </a>
-
-  </div>
+      <SmallBanner class="Banner"><a href="https://cryptovision.live" title="Cryptovision.live">
+        <img src={require('url:../../assets/cvn.png')} />
+      </a></SmallBanner>
+    </Container>
+  )
 }
